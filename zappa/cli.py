df50774fe221c9d06d3695ff5a4e52f8da8422c7
istamqulov <istamqulov@gmail.com>
2021-09-29 19:51:11 +0500

    merged zappa

--- zappa/cli.py
+++ zappa/cli.py
@@ -997,7 +997,7 @@ class ZappaCLI:
 
             if self.stage_config.get("touch", True):
                 self.zappa.wait_until_lambda_function_is_ready(
-                    function_name=self.lambda_name
+                    lambda_arn=self.lambda_arn
                 )
                 self.touch_endpoint(endpoint_url)
 
@@ -1140,9 +1140,7 @@ class ZappaCLI:
         if docker_image_uri:
             kwargs["docker_image_uri"] = docker_image_uri
             self.lambda_arn = self.zappa.update_lambda_function(**kwargs)
-            self.zappa.wait_until_lambda_function_is_ready(
-                function_name=self.lambda_name
-            )
+            self.zappa.wait_until_lambda_function_is_ready(lambda_arn=self.lambda_arn)
         elif source_zip and source_zip.startswith("s3://"):
             bucket, key_name = parse_s3_url(source_zip)
             kwargs.update(dict(bucket=bucket, s3_key=key_name))
